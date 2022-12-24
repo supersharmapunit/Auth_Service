6fff39b0af3681af8fa785d55638deb613e7dd66
supersharmapunit <punit2000sharma@gmail.com>
2022-12-25 01:22:38 +0530

    Added user isAuth function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,4 +8,6 @@ const router = express.Router();
 router.post('/signup', AuthRequestValidator.validateUserAuth, UserController.create);
 router.post('/signin', AuthRequestValidator.validateUserAuth, UserController.signIn);
 
+router.get('/isAuthenticated', UserController.isAuthenticated);
+
 module.exports = router;
